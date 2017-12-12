25fbfdff58e259be62a20ce8154c31a9659226fd
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:16:37 +0100

    fini

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 4 Exercice 2</title>
+  </head>
+  <body>
+    <?php
+    $chaine = 'Salut';
+    function returnString($chaine){
+      return $chaine;
+    }
+    echo returnString($chaine);
+     ?>
+  </body>
+</html>
